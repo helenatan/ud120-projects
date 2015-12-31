90f378f79ee281a32f59b59a0f0dcadf308e981a
Helena Tan <htan@sfo-m-2012.local>
2015-12-31 02:15:21 -0500

    ml_dec30

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -30,7 +30,9 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
-
+from sklearn.naive_bayes import GaussianNB
+clf=GaussianNB()
+clf.fit(features_train,labels_train)
 
 
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+
+
+from sklearn.svm import SVC
+clf = SVC(kernel='rbf', C=10000)
+clf.fit(features_train,labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred, labels_test)
+
+print accuracy
+
+
 #########################################################
 
 
